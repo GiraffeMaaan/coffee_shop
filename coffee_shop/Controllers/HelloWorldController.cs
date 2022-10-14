4ccda3c61615184df3f9839b3cc1ffc60c4f2996
shige_win <tomoshige124@gmail.com>
2022-10-14 21:17:06 +0900

    初回コミット

---
+++ coffee_shop/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace coffeeshop.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string welcome()
+        {
+            return "Welcomeページです";
+        }
+    }
+}
